d3918a42ce1e3f2f509e56f3ca8ab392f673f371
asadawan16 <asadawan16900@gmail.com>
2025-02-25 22:17:16 +0500

    updated backend url

--- Frontend/src/utils/axiosInstance/axiosInstance.js
+++ Frontend/src/utils/axiosInstance/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://reveify-mern-app-backend.vercel.app/api",
   withCredentials: true,
 });
 
