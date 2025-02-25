c46721758d80128e75b264dedabfb42bcbbee252
asadawan16 <asadawan16900@gmail.com>
2025-02-25 22:10:57 +0500

    removed unused fonts

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,10 @@ const ReviewsRoutes = require("./routes/reviews");
 const FetchUserRoute = require("./routes/user/fetchUsers");
 const PORT = process.env.PORT || 5000;
 // Cors Configuration
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://reveify-mern-app-frontend.vercel.app",
+];
 app.use(
   cors(
     {
