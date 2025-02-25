32d04850a0fb7cefbe91a2d3e60256a3f0f04811
asadawan16 <asadawan16900@gmail.com>
2025-02-25 22:08:01 +0500

    removed unused fonts

--- Frontend/src/components/Review/Review.jsx
+++ Frontend/src/components/Review/Review.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../header/header";
-import Banner from "./Banner";
+import Banner from "./banner";
 import Footer from "../footer/footer";
 import ReviewForm from "./reviewForm";
 const Review = () => {
