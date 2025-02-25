d4698da208483e0ec0a838df51e43309dd91a5bc
asadawan16 <asadawan16900@gmail.com>
2025-02-26 00:16:47 +0500

    CORS

--- Backend/index.js
+++ Backend/index.js
@@ -19,6 +19,7 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     AccessControlAllowCredentials: true,
+    AccessControlAllowOrigin: allowedOrigins,
     exposedHeaders: ["Authorization"],
   })
 );
