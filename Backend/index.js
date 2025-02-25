96ef6ee199bb3bb932b1a3032f52229e8500171f
asadawan16 <asadawan16900@gmail.com>
2025-02-25 23:10:50 +0500

    CORS UPDATE

--- Backend/index.js
+++ Backend/index.js
@@ -8,28 +8,20 @@ const ReviewsRoutes = require("./routes/reviews");
 const FetchUserRoute = require("./routes/user/fetchUsers");
 const PORT = process.env.PORT || 5000;
 // Cors Configuration
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  next();
-});
-
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
       "https://reveify-mern-app-frontend.vercel.app",
+      "https://reveify-mern-app.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
+    exposedHeaders: ["Authorization"],
   })
 );
-
+app.options("*", cors());
 // Mongodb Connection
 app.use(express.json());
 mongoose
--- Frontend/src/utils/axiosInstance/axiosInstance.js
+++ Frontend/src/utils/axiosInstance/axiosInstance.js
@@ -1,8 +1,10 @@
-import axios from "axios";
-
 const API = axios.create({
   baseURL: "https://reveify-mern-app-backend.vercel.app/api",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
 
 export default API;
