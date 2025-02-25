8746391c7a070f83eae1349f226f7ba5c0bc1b87
asadawan16 <asadawan16900@gmail.com>
2025-02-25 23:23:46 +0500

    CORS UPDATE

--- Backend/index.js
+++ Backend/index.js
@@ -10,11 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Cors Configuration
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://reveify-mern-app-frontend.vercel.app",
-      "https://reveify-mern-app.vercel.app",
-    ],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
