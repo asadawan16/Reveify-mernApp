3b06a0ec0cc035d6ae33625ecb3dbebf28618753
asadawan16 <asadawan16900@gmail.com>
2025-02-27 12:33:38 +0500

    footer updated

--- Frontend/src/components/footer/footer.jsx
+++ Frontend/src/components/footer/footer.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./footer.module.css";
 const Footer = () => {
+  const role = sessionStorage.getItem("user");
+
   return (
     <footer>
       <div className={classes.container}>
         <h2>Reviefy</h2>
-        <ul>
-          <li>
-            <Link to="/">Reviews</Link>
-          </li>
-          <li>
-            <Link to="/user-dashboard">Dashboard</Link>
-          </li>
-        </ul>
+        {role === "user " && (
+          <ul>
+            <li>
+              <Link to="/">Reviews</Link>
+            </li>
+            <li>
+              <Link to="/user-dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        )}
       </div>
       <span className={classes.copyright}>&copy; 2023 Reviefy</span>
     </footer>
